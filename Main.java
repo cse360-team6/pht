5f4e7e15b458586ac8de985f41fc84434c5f1d1e
acdent <acdent@asu.edu>
2014-03-05 21:11:26 -0700

    * Current implementation of Activity window -- with main class

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		PhysicalActivity physical = new PhysicalActivity();
+		physical.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		physical.setVisible(true);
+		
+		//HealthInformation health = new HealthInformation();		
+		//health.setVisible(true);
+		
+		
+	}
+
+}
