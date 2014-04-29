ce6334557c5e91e775cfff5cd0f65aac7de1afd0
Your Name Here <dbixler@asu.edu>
2014-04-28 21:58:59 -0700

    .

--- src/org/pht/user/data/Quota.java
+++ src/org/pht/user/data/Quota.java
@@ -12,12 +12,35 @@ public class Quota implements Serializable {
 	/*
 	 * Only if we can get that far private int systolicGoal, diastolicGoal,
 	 * heartRateGoal, systolicWeeklyProgress, diastolicWeeklyProgress,
-	 * heartRateWeeklyProgress; 
-	 * private double bloodSugarGoal, bloodSugarWeeklyProgress;
+	 * heartRateWeeklyProgress; private double bloodSugarGoal,
+	 * bloodSugarWeeklyProgress;
 	 */
 
 	public final static int STRENGTH = 0, CARDIO = 1, WORK = 2, SLEEP = 3;
 
+////	public void main(String args[]) {
+//		Calendar cal = Calendar.getInstance();
+//		Data data = new Data();
+//		int[] actual = { 0, 0, 0, 0 };
+//		int[] expected = { 0, 0, 0, 0 };
+//		for (int i = 0; i < 20; i++) {
+//			cal.set(Calendar.DAY_OF_YEAR, i);
+//			data.addEntry(new DataEntry(Calendar.getInstance(), i * 1, i * 2,
+//					i * 3, i * 4, i * 5, i * 6, i * 7, i * 1.1));
+//		}
+//		
+//		for (int i = 19; i <= 20; i++) {
+//			expected[0] += i*1;
+//			expected[1] += i*2;
+//			expected[2] += i*3;
+//			expected[3] += i*4;
+//		}
+//		
+//		for(int i = 0; i < 4; i++) {
+//			System.out.println(expected[i]);
+//		}
+//	}
+	
 	public Quota() {
 
 	}
@@ -28,11 +51,10 @@ public class Quota implements Serializable {
 		workWeeklyProgress = 0;
 		sleepWeeklyProgress = 0;
 		Calendar today = Calendar.getInstance();
-		if (d == null)
-			return;
 		for (int i = Calendar.getInstance().get(Calendar.DAY_OF_WEEK); i > -1; i--, today
 				.set(Calendar.DAY_OF_WEEK, i)) {
 			DataEntry tmp = d.getEntry(today);
+			if (tmp == null) continue;
 			this.strengthWeeklyProgress += tmp.getStrengthHours();
 			this.cardioWeeklyProgress += tmp.getCardioHours();
 			this.workWeeklyProgress += tmp.getWorkHours();
@@ -113,73 +135,74 @@ public class Quota implements Serializable {
 	public void setSleepGoal(int sleepGoal) {
 		this.sleepGoal = sleepGoal;
 	}
-	
+
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
 
-//	public int getSystolicWeeklyProgress() {
-//		return systolicWeeklyProgress;
-//	}
-//
-//	public void setSystolicWeeklyProgress(int systolicWeeklyProgress) {
-//		this.systolicWeeklyProgress = systolicWeeklyProgress;
-//	}
-//
-//	public int getDiastolicWeeklyProgress() {
-//		return diastolicWeeklyProgress;
-//	}
-//
-//	public void setDiastolicWeeklyProgress(int diastolicWeeklyProgress) {
-//		this.diastolicWeeklyProgress = diastolicWeeklyProgress;
-//	}
-//
-//	public int getHeartRateWeeklyProgress() {
-//		return heartRateWeeklyProgress;
-//	}
-//
-//	public void setHeartRateWeeklyProgress(int heartRateWeeklyProgress) {
-//		this.heartRateWeeklyProgress = heartRateWeeklyProgress;
-//	}
-//
-//	public int getSystolicGoal() {
-//		return systolicGoal;
-//	}
-//
-//	public void setSystolicGoal(int systolicGoal) {
-//		this.systolicGoal = systolicGoal;
-//	}
-//
-//	public int getDiastolicGoal() {
-//		return diastolicGoal;
-//	}
-//
-//	public void setDiastolicGoal(int diastolicGoal) {
-//		this.diastolicGoal = diastolicGoal;
-//	}
-//
-//	public int getHeartRateGoal() {
-//		return heartRateGoal;
-//	}
-//
-//	public void setHeartRateGoal(int heartRateGoal) {
-//		this.heartRateGoal = heartRateGoal;
-//	}
-//
-//	public double getBloodSugarGoal() {
-//		return bloodSugarGoal;
-//	}
-//
-//	public void setBloodSugarGoal(double bloodSugarGoal) {
-//		this.bloodSugarGoal = bloodSugarGoal;
-//	}
-//
-//
-//	public double getBloodSugarWeeklyProgress() {
-//		return bloodSugarWeeklyProgress;
-//	}
-//
-//	public void setBloodSugarWeeklyProgress(double bloodSugarWeeklyProgress) {
-//		this.bloodSugarWeeklyProgress = bloodSugarWeeklyProgress;
-//	}
+	// public int getSystolicWeeklyProgress() {
+	// return systolicWeeklyProgress;
+	// }
+	//
+	// public void setSystolicWeeklyProgress(int systolicWeeklyProgress) {
+	// this.systolicWeeklyProgress = systolicWeeklyProgress;
+	// }
+	//
+	// public int getDiastolicWeeklyProgress() {
+	// return diastolicWeeklyProgress;
+	// }
+	//
+	// public void setDiastolicWeeklyProgress(int diastolicWeeklyProgress) {
+	// this.diastolicWeeklyProgress = diastolicWeeklyProgress;
+	// }
+	//
+	// public int getHeartRateWeeklyProgress() {
+	// return heartRateWeeklyProgress;
+	// }
+	//
+	// public void setHeartRateWeeklyProgress(int heartRateWeeklyProgress) {
+	// this.heartRateWeeklyProgress = heartRateWeeklyProgress;
+	// }
+	//
+	// public int getSystolicGoal() {
+	// return systolicGoal;
+	// }
+	//
+	// public void setSystolicGoal(int systolicGoal) {
+	// this.systolicGoal = systolicGoal;
+	// }
+	//
+	// public int getDiastolicGoal() {
+	// return diastolicGoal;
+	// }
+	//
+	// public void setDiastolicGoal(int diastolicGoal) {
+	// this.diastolicGoal = diastolicGoal;
+	// }
+	//
+	// public int getHeartRateGoal() {
+	// return heartRateGoal;
+	// }
+	//
+	// public void setHeartRateGoal(int heartRateGoal) {
+	// this.heartRateGoal = heartRateGoal;
+	// }
+	//
+	// public double getBloodSugarGoal() {
+	// return bloodSugarGoal;
+	// }
+	//
+	// public void setBloodSugarGoal(double bloodSugarGoal) {
+	// this.bloodSugarGoal = bloodSugarGoal;
+	// }
+	//
+	//
+	// public double getBloodSugarWeeklyProgress() {
+	// return bloodSugarWeeklyProgress;
+	// }
+	//
+	// public void setBloodSugarWeeklyProgress(double bloodSugarWeeklyProgress)
+	// {
+	// this.bloodSugarWeeklyProgress = bloodSugarWeeklyProgress;
+	// }
 }
