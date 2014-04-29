c42445f7e01d4e377fe9e3b1070f6e87eef07e3b
Trenton Smith <tlsdude@gmail.com>
2014-04-28 21:38:49 -0700

    TAKE MY STUFF

--- src/org/pht/PersonalHealthTracker.java
+++ src/org/pht/PersonalHealthTracker.java
@@ -1,27 +1,14 @@
 package org.pht;
 
-import java.io.IOException;
-
 import org.pht.ui.MainFrame;
-import org.pht.ui.activity.HealthActivityFrame;
-import org.pht.ui.activity.PhysicalActivityFrame;
-import org.pht.user.User.Gender;
-import org.pht.user.Users;
-
-import javax.swing.JFrame;
 
 public class PersonalHealthTracker
 {
+	private static MainFrame MAINFRAME;
     public static void main(String[] args)
-    {
-    	try {
-			Users usr = new Users();
-			usr.createUser("hi", Gender.MALE, 55, 180, 18);
-		} catch (ClassNotFoundException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	
-        MainFrame mainFrame = new MainFrame();
+    {    	
+        MAINFRAME = new MainFrame();
     }
+    
+    public static MainFrame getMainFrame() {return MAINFRAME;}
 }
