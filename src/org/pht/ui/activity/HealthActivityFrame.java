13c5fe0bf96ab0230eabeac6c8aba90047b6516c
Trenton Smith <tlsdude@gmail.com>
2014-05-02 16:23:51 -0700

    Made my test case diagram easy.

--- src/org/pht/ui/activity/HealthActivityFrame.java
+++ src/org/pht/ui/activity/HealthActivityFrame.java
@@ -51,120 +51,17 @@ public class HealthActivityFrame extends ActivityFrame {
 	            	
 	            	//If the user is entering Blood Pressure
 	            	if(((String) activities.getSelectedItem()).equals(healthInformation[0])) {
-	            		ArrayList<String> bp = new ArrayList<String>(Arrays.asList(value.getText().split("/")));
-	            		if (bp.size() < 2) {
-	            			notice.setText("Error: Invalid Blood Pressure Sys/Dia");
-	            		} else {		 
-	            			int s, d;
-		            		try {
-		            			s = Integer.parseInt(bp.get(0));
-		            			d = Integer.parseInt(bp.get(1));
-		            		} catch(Exception exc) {
-		            			notice.setText("Error: Invalid Blood Pressure Sys/Dia");
-		            			return;
-		            		}
-		            		
-		            		if(s < 240 && s > 0 && d < 240 && d > 0) {
-		            			systolic = s;
-		            			diastolic = d;
-		            			memoStored = memo.getText();
-		            			typeStored = (String)activities.getSelectedItem();
-		            			if (dateChooser.getCalendar() == null){
-									notice.setText("  Error: Please Select Date");
-									return;
-								} else if (dateChooser.getCalendar().compareTo(Calendar.getInstance()) > 0) {
-									notice.setText("  Error: Please Select Valid Date");
-									return;
-								}
-				            	dateAdded = dateChooser.getCalendar();
-		            			User user = PersonalHealthTracker.getMainFrame().getUsers().getUsers().get(PersonalHealthTracker.getMainFrame().getCurrentUser());
-		            			DataEntry de = user.getData().getEntry(dateAdded);
-		            			if (de == null)
-		            				user.getData().addEntry(dateAdded, 0, 0, 0, 0, s, d, 0, 0, memoStored);
-		            			else {
-		            				de.setSystolic(s);
-		            				de.setDiastolic(d);
-		            			}
-		            			dispose();
-		            		} else {
-		            			notice.setText("Error: Invalid Blood Pressure Sys/Dia");
-		            			return;		            			
-		            		}
-	            		}
+	            		addBloodPressure();
 	            	}
      	
 	            	//If the user is entering Blood Sugar
 	            	if(((String) activities.getSelectedItem()).equals(healthInformation[1])) {
-	            		
-	            		int bs;
-	            		try {
-	            			bs = Integer.parseInt(value.getText());
-	            		} catch (Exception exc) {
-	            			notice.setText("Error: Invalid Blood Sugar Value");
-	            			return;
-	            		}
-	            		
-	            		//If the entered values are within a valid range, parse the strings for the integer values and close the window
-	            		if(bs > 0 && bs < 400) {
-	            			bloodSugar = bs;
-	            			memoStored = memo.getText();
-	    	            	typeStored = (String) activities.getSelectedItem();
-	    	            	if (dateChooser.getCalendar() == null){
-								notice.setText("  Error: Please Select Date");
-								return;
-							} else if (dateChooser.getCalendar().compareTo(Calendar.getInstance()) > 0) {
-								notice.setText("  Error: Please Select Valid Date");
-								return;
-							}
-			            	dateAdded = dateChooser.getCalendar();
-	            			User user = PersonalHealthTracker.getMainFrame().getUsers().getUsers().get(PersonalHealthTracker.getMainFrame().getCurrentUser());
-	            			DataEntry de = user.getData().getEntry(dateAdded);
-	            			if (de == null)
-	            				user.getData().addEntry(dateAdded, 0, 0, 0, 0, 0, 0, 0, bloodSugar, memoStored);
-	            			else
-	            				de.setBloodSugar(bloodSugar);
-	            			dispose();
-	            		} else {
-	            			notice.setText("Error: Invalid Blood Sugar Value");
-	            			return;
-	            		}
+	            		addBloodSugar();
 	            	}
 	            	
 	            	//If the user is entering Heart Rate
 	            	if(((String) activities.getSelectedItem()).equals(healthInformation[2])) {
-	            		
-	            		int hr;
-	            		try {
-	            			hr = Integer.parseInt(value.getText());
-	            		} catch (Exception exc) {
-	            			notice.setText("Error: Invalid Heart Rate Value");
-	            			return;
-	            		}
-	            		if (dateChooser.getCalendar() == null){
-							notice.setText("  Error: Please Select Date");
-							return;
-						} else if (dateChooser.getCalendar().compareTo(Calendar.getInstance()) > 0) {
-							notice.setText("  Error: Please Select Valid Date");
-							return;
-						}
-		            	dateAdded = dateChooser.getCalendar();
-	            		
-	            		//If the entered values are within a valid range, parse the strings for the integer values and close the window
-	            		if(hr > 0 && hr < 300) {
-	            			heartRate = hr;
-	            			memoStored = memo.getText();
-	    	            	typeStored = (String) activities.getSelectedItem();
-	    	            	User user = PersonalHealthTracker.getMainFrame().getUsers().getUsers().get(PersonalHealthTracker.getMainFrame().getCurrentUser());
-	    	            	DataEntry de = user.getData().getEntry(dateAdded);
-	            			if (de == null)
-	            				user.getData().addEntry(dateAdded, 0, 0, 0, 0, 0, 0, heartRate, 0, memoStored);
-	            			else
-	            				de.setRestingHeartRate(heartRate);
-	    	            	dispose();	    	            	
-	            		} else {
-	            			notice.setText("Error: Invalid Blood Sugar Value");
-	            			return;
-	            		}
+	            		addHeartRate();
 	            	}
 	            }
 	        }
@@ -202,20 +99,120 @@ public class HealthActivityFrame extends ActivityFrame {
 		return healthComponent;
     }
 
-    //Accessors
-	public int getSystolic() {
-		//Return Blood Pressure entered
-		return systolic;
-	}
-	public int getDiastolic() {
-		return diastolic;
-	}
-	public int getBloodSugar() {
-		//Return Blood Sugar entered
-		return bloodSugar;
-	}
-	public int getHeartRate() {
-		//Return Heart Rate entered
-		return heartRate;
-	}
+    private boolean addHeartRate() {
+    	int hr;
+		try {
+			hr = Integer.parseInt(value.getText());
+		} catch (Exception exc) {
+			notice.setText("Error: Invalid Heart Rate Value");
+			return false;
+		}
+		if (dateChooser.getCalendar() == null){
+			notice.setText("  Error: Please Select Date");
+			return false;
+		} else if (dateChooser.getCalendar().compareTo(Calendar.getInstance()) > 0) {
+			notice.setText("  Error: Please Select Valid Date");
+			return false;
+		}
+    	dateAdded = dateChooser.getCalendar();
+		
+		//If the entered values are within a valid range, parse the strings for the integer values and close the window
+		if(hr > 0 && hr < 300) {
+			heartRate = hr;
+			memoStored = memo.getText();
+        	typeStored = (String) activities.getSelectedItem();
+        	User user = PersonalHealthTracker.getMainFrame().getUsers().getUsers().get(PersonalHealthTracker.getMainFrame().getCurrentUser());
+        	DataEntry de = user.getData().getEntry(dateAdded);
+			if (de == null)
+				user.getData().addEntry(dateAdded, 0, 0, 0, 0, 0, 0, heartRate, 0, memoStored);
+			else
+				de.setRestingHeartRate(heartRate);
+        	dispose();	    	            	
+		} else {
+			notice.setText("Error: Invalid Blood Sugar Value");
+			return false;
+		}
+		return true;
+    	
+    }
+
+    private boolean addBloodPressure() {
+    	ArrayList<String> bp = new ArrayList<String>(Arrays.asList(value.getText().split("/")));
+		if (bp.size() < 2) {
+			notice.setText("Error: Invalid Blood Pressure Sys/Dia");
+		} else {		 
+			int s, d;
+    		try {
+    			s = Integer.parseInt(bp.get(0));
+    			d = Integer.parseInt(bp.get(1));
+    		} catch(Exception exc) {
+    			notice.setText("Error: Invalid Blood Pressure Sys/Dia");
+    			return false;
+    		}
+    		
+    		if(s < 240 && s > 0 && d < 240 && d > 0) {
+    			systolic = s;
+    			diastolic = d;
+    			memoStored = memo.getText();
+    			typeStored = (String)activities.getSelectedItem();
+    			if (dateChooser.getCalendar() == null){
+					notice.setText("  Error: Please Select Date");
+					return false;
+				} else if (dateChooser.getCalendar().compareTo(Calendar.getInstance()) > 0) {
+					notice.setText("  Error: Please Select Valid Date");
+					return false;
+				}
+            	dateAdded = dateChooser.getCalendar();
+    			User user = PersonalHealthTracker.getMainFrame().getUsers().getUsers().get(PersonalHealthTracker.getMainFrame().getCurrentUser());
+    			DataEntry de = user.getData().getEntry(dateAdded);
+    			if (de == null)
+    				user.getData().addEntry(dateAdded, 0, 0, 0, 0, s, d, 0, 0, memoStored);
+    			else {
+    				de.setSystolic(s);
+    				de.setDiastolic(d);
+    			}
+    			dispose();
+    		} else {
+    			notice.setText("Error: Invalid Blood Pressure Sys/Dia");
+    			return false;		            			
+    		}
+		}
+		return true;
+    }
+
+    private boolean addBloodSugar() {
+    	int bs;
+		try {
+			bs = Integer.parseInt(value.getText());
+		} catch (Exception exc) {
+			notice.setText("Error: Invalid Blood Sugar Value");
+			return false;
+		}
+		
+		//If the entered values are within a valid range, parse the strings for the integer values and close the window
+		if(bs > 0 && bs < 400) {
+			bloodSugar = bs;
+			memoStored = memo.getText();
+        	typeStored = (String) activities.getSelectedItem();
+        	if (dateChooser.getCalendar() == null){
+				notice.setText("  Error: Please Select Date");
+				return false;
+			} else if (dateChooser.getCalendar().compareTo(Calendar.getInstance()) > 0) {
+				notice.setText("  Error: Please Select Valid Date");
+				return false;
+			}
+        	dateAdded = dateChooser.getCalendar();
+			User user = PersonalHealthTracker.getMainFrame().getUsers().getUsers().get(PersonalHealthTracker.getMainFrame().getCurrentUser());
+			DataEntry de = user.getData().getEntry(dateAdded);
+			if (de == null)
+				user.getData().addEntry(dateAdded, 0, 0, 0, 0, 0, 0, 0, bloodSugar, memoStored);
+			else
+				de.setBloodSugar(bloodSugar);
+			dispose();
+		} else {
+			notice.setText("Error: Invalid Blood Sugar Value");
+			return false;
+		}
+		return true;
+    }
 }
